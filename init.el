8e6d7c01baba28e32834b5cd614e378bcd7eb763
mkra0004 <marcin.krasowski@synchronoss.com>
2017-11-01 21:11:13 +0000

    c

--- init.el
+++ init.el
@@ -1,12 +1,7 @@
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (defconst conf-dir "~/prj/misc/emacsrc")
-(org-babel-load-file "~/prj/emacsrc/other-init.el.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,10 +9,12 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode xcscope projectile material-theme magit lua-mode kotlin-mode jedi groovy-mode elpy cider better-defaults))))
+    (dockerfile-mode yaml-mode xcscope projectile material-theme magit lua-mode kotlin-mode jedi groovy-mode elpy cider better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(org-babel-load-file (concat conf-dir "/other-init.el.org"))

ee5dd78fafd7a5a92ade2253424354fe2c8ca736
krasm <mkrasowski@gmail.com>
2018-03-14 21:00:48 +0000

    initial seting for org mode

--- init.el
+++ init.el
@@ -3,13 +3,17 @@
 (defconst conf-dir "~/prj/emacsrc")
 (defconst snippets-dir "~/.emacs.d/yasnippet-snippets")
 (defconst packages-dir "~/prj/emacsrc/packages")
-(defconst aspell-prog "/usr/local/bin/aspell")
+(defconst aspell-prog "/usr/bin/aspell")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(nxml-attribute-indent 2)
+ '(nxml-auto-insert-xml-declaration-flag t)
+ '(nxml-bind-meta-tab-to-complete-flag t)
+ '(nxml-slash-auto-complete-flag t)
  '(package-selected-packages
    (quote
     (dockerfile-mode yaml-mode xcscope projectile material-theme magit lua-mode kotlin-mode jedi groovy-mode elpy cider better-defaults))))

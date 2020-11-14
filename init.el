4da4c686e908038070c3e9c270e5d8be353cb697
marcin <m@krasm.net>
2020-11-14 19:21:26 +0000

    do not force to refresh packages

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-(package-initialize)
+(if (< emacs-major-version 27)
+    (package-initialize)
+  )
 
 (defconst conf-dir (getenv "EMACS_CONF_DIR"))
 (defconst snippets-dir "~/.emacs.d/yasnippet-snippets")
@@ -12,14 +14,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eclim-eclipse-dirs (quote ("~/opt/sts-bundle/sts-3.9.0-RELEASE")))
+ '(eclim-eclipse-dirs '("~/opt/sts-bundle/sts-3.9.0-RELEASE"))
  '(nxml-attribute-indent 2)
  '(nxml-auto-insert-xml-declaration-flag t)
  '(nxml-bind-meta-tab-to-complete-flag t)
  '(nxml-slash-auto-complete-flag t)
  '(package-selected-packages
-   (quote
-    (lua-mode yaml-mode xcscope solarized-theme projectile powerline org-bullets magit evil elpy dockerfile-mode better-defaults auto-complete))))
+   '(lua-mode yaml-mode xcscope solarized-theme projectile powerline org-bullets magit evil elpy dockerfile-mode better-defaults auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

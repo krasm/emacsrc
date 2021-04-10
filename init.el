5228483d86e1030a4af2617cdc47ec6ce3afab47
krasm <m@krasm.net>
2021-04-10 19:49:47 +0100

    updates to installation process

--- init.el
+++ init.el
@@ -14,7 +14,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eclim-eclipse-dirs '("~/opt/sts-bundle/sts-3.9.0-RELEASE"))
  '(nxml-attribute-indent 2)
  '(nxml-auto-insert-xml-declaration-flag t)
  '(nxml-bind-meta-tab-to-complete-flag t)
--- install.sh
+++ install.sh
@@ -1,7 +1,41 @@
 #!/bin/sh
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y autoconf build-essential texinfo libgnutls28-dev libgnutls30 libxml2-dev libxml2-utils gnutls-bin libncurses5-dev
+
+exists () {
+  type "$1" >/dev/null 2>/dev/null
+}
+
+exists "apt"
+APT=$?
+if [ "x0" = "x"$APT ]; then
+	sudo apt update && sudo apt upgrade -y
+	if [ "x0" != "x"$? ]; then
+		echo "update failed - quitting"
+		exit 1
+	fi
+	sudo apt install -y autoconf build-essential texinfo libgnutls28-dev libgnutls30 libxml2-dev libxml2-utils gnutls-bin libncurses5-dev pkg-config
+	if [ "x0" != "x"$? ]; then
+		echo "update failed - quitting"
+		exit 1
+	fi
+fi
+
+if [ -d $HOME/.emacs.d ]; then
+	echo ".emacs.d found - quitting"
+	exit 1
+fi
+
+mkdir -p $HOME/.emacs.d/
+ln -s $PWD/init.el $HOME/.emacs.d/init.el
+ln -s $PWD/yasnippet-snipets $HOME/.emacs.d/
+
 
 git clone https://git.savannah.gnu.org/git/emacs.git /tmp/emacs
-(cd /tmp/emacs && git checkout tags/emacs-26.1 && ./autogen.sh && \
-	./configure --prefix=$HOME/opt/emacs --with-wide-int --without-x --without-libsystemd --with-xml2 --without-gpm  --with-zlib --with-gnutls && make && make install)
+(cd /tmp/emacs && git checkout tags/emacs-27.2 && ./autogen.sh && \
+	./configure --prefix=$HOME/opt/emacs --with-wide-int --without-x --without-libsystemd --with-xml2 --without-gpm  --with-zlib --with-gnutls --with-json && make && make install)
+
+if [ "x0" = "x"$? ] ; then
+	mkdir $HOME/bin/
+	ln -s $HOME/opt/emacs/bin/* $HOME/bin
+fi
+
+

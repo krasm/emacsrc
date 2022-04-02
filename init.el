266870b0601f6b501ceba7a38529f02ea9a389c5
msk <m@krasm.net>
2022-04-02 18:02:38 +0100

    Fixes and package cleanup

--- init.el
+++ init.el
@@ -18,8 +18,7 @@
  '(nxml-auto-insert-xml-declaration-flag t)
  '(nxml-bind-meta-tab-to-complete-flag t)
  '(nxml-slash-auto-complete-flag t)
- '(package-selected-packages
-   '(lua-mode yaml-mode xcscope solarized-theme projectile powerline org-bullets magit evil elpy dockerfile-mode better-defaults auto-complete)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

d1d9b144f3af7550a8f1bba9cd1a9cc94cec0ce8
msk <m@krasm.net>
2023-03-30 20:23:45 +0100

    added lsp-java

--- init.el
+++ init.el
@@ -16,11 +16,15 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" default))
+ '(frame-background-mode nil)
+ '(mail-user-agent 'gnus-user-agent)
  '(nxml-attribute-indent 2)
  '(nxml-auto-insert-xml-declaration-flag t)
  '(nxml-bind-meta-tab-to-complete-flag t)
  '(nxml-slash-auto-complete-flag t)
- )
+ '(package-selected-packages
+   '(notmuch elpy yasnippet-snippets yaml-mode xcscope restclient projectile org-bullets dockerfile-mode better-defaults auto-complete))
+ '(send-mail-function 'sendmail-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

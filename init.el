856ee3e4f57a24c0ad1d89fda53f65999a4a7ce1
msk <m@krasm.net>
2025-03-27 08:14:22 +0100

    clean up

--- init.el
+++ init.el
@@ -3,28 +3,35 @@
     (package-initialize)
   )
 
-(defconst conf-dir (getenv "EMACS_CONF_DIR"))
-(defconst snippets-dir "~/.emacs.d/yasnippet-snippets")
-(defconst packages-dir (concat conf-dir "/packages"))
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+     (lua "https://github.com/tree-sitter-grammars/tree-sitter-lua")
+     (c "https://github.com/tree-sitter/tree-sitter-c")
+     (c++ "https://github.com/tree-sitter/tree-sitter-cpp")
+     )
+   )
 
+(load "~/.emacs.d/sanemacs.el" nil t)
 
-(org-babel-load-file (concat conf-dir "/emacs-customize.el.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" default))
- '(frame-background-mode nil)
- '(mail-user-agent 'gnus-user-agent)
- '(nxml-attribute-indent 2)
- '(nxml-auto-insert-xml-declaration-flag t)
- '(nxml-bind-meta-tab-to-complete-flag t)
- '(nxml-slash-auto-complete-flag t)
- '(package-selected-packages
-   '(notmuch elpy yasnippet-snippets yaml-mode xcscope restclient projectile org-bullets dockerfile-mode better-defaults auto-complete))
- '(send-mail-function 'sendmail-send-it))
+ '(package-vc-selected-packages
+   '((copilot :url "https://github.com/copilot-emacs/copilot.el" :branch
+	      "main"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
